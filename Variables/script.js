0de4cf35e7ea6b26f30fcec181082312d43bf0ac
Yeimer Pinedo <pinedoy@globalhitss.com>
2024-02-29 15:50:03 -0500

    Variables

---
+++ Variables/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let admin = '';
+let name = 'Jhon';
+admin = name;
+alert(admin);
+
+let planetName = 'Tierra';
+let currentUser = 'Usuario';
+
+const BIRTHDAY = '15.02.1994'
+const age = someCode(BIRTHDAY);
+
+alert(age);
